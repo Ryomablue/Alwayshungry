2dfb127456390dd2ece2efca5697006be55d396e
Ryomablue <flclfan12@gmail.com>
2016-09-06 18:21:31 +0000

    Lesson 24

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   
   def index
-    @places = Place.paginate(:page => params[:page], :per_page => 5)
+    @places = Place.order("created_at ASC").all.paginate(page: params[:page], per_page:5)
   end
   
   def new
@@ -16,6 +16,22 @@ class PlacesController < ApplicationController
   
   def show
     @place = Place.find(params[:id])
+  end
+  
+  def edit
+    @place = Place.find(params[:id])
+  end
+  
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
+  
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+    redirect_to root_path
   end
   
   private
