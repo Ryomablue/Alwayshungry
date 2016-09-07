dfebb97cbb6a10546fd14848f65f51dcbff3a08a
Ryomablue <flclfan12@gmail.com>
2016-09-07 00:02:05 +0000

    Lesson 29

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ gem 'simple_form'
 gem 'sprockets', '2.11.0'
 gem 'will_paginate', '~> 3.1.0'
 gem 'devise', '>=3.2.4'
+gem 'geocoder', '1.3.4'
+gem 'figaro', '>=1.0.0'
 
 gem 'rails_12factor', group: :production
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,9 @@
 class Place < ActiveRecord::Base
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode
+  
   validates :name, length: {minimum: 3}, presence: true
-  validates :address, presence: true
-  validates :description, presence: true
+  validates :address, length: {minimum: 10}, presence: true
+  validates :description, length: {maximum: 500}, presence: true
 end
