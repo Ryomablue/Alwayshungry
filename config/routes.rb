076e609241493bdd7cbd3ac2d7120bea25e22244
Ryomablue <flclfan12@gmail.com>
2016-09-10 15:26:41 +0000

    Lesson 35

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'places#index'
   
-  resources :places
-  resources :comments
+  resources :places do
+    resources :comments, only: :create
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
